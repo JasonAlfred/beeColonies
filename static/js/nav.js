762517e79e13449a07def6accecc40da30abfe3d
AliAnderson <alexis.anderson@target.com>
2020-10-16 15:22:22 -0500

    add some outside libraries necessary for the interactive state map

--- static/js/nav.js
+++ static/js/nav.js
@@ -8,4 +8,38 @@ function getBeeData() {
     })
 }
 
+$(document).ready(function() {
+    $('#usaMap').usmap({
+      'stateSpecificStyles': {
+        'AK' : {fill: '#0099CC'}
+      },
+      'stateSpecificHoverStyles': {
+        'HI' : {fill: '#0099CC'}
+      },
+      
+      'mouseoverState': {
+        'HI' : function(event, data) {
+          //return false;
+        }
+      },
+      
+      
+      'click' : function(event, data) {
+        $('#alert')
+          .text('Click '+data.name+' on map 1')
+          .stop()
+          .css('backgroundColor', '#0099CC')
+          .animate({backgroundColor: '#0099CC'}, 1000);
+      }
+    });
+    
+    $('#over-md').click(function(event){
+      $('#map').usmap('trigger', 'MD', 'mouseover', event);
+    });
+    
+    $('#out-md').click(function(event){
+      $('#map').usmap('trigger', 'MD', 'mouseout', event);
+    });
+  });
+
 getBeeData();
