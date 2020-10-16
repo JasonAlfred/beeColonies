091b9b3c72919a9c909c556816cf45e4105b5eb6
AliAnderson <alexis.anderson@target.com>
2020-10-16 12:07:45 -0500

    slight change in how we call API so that we can use Live Server while working

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,7 +1,16 @@
+var beeData;
+
 function getBeeData() {
+<<<<<<< Updated upstream
     let url = `http://127.0.0.1:5000/data_api`;
     let dict = d3.json(url).then(function (data) {
         console.log(data);
+=======
+    let dict = d3.json("/data_api").then(function(data) {
+        beeData = data;
+        console.log(beeData);
+
+>>>>>>> Stashed changes
     })
 }
 
