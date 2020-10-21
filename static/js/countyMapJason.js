75acdc12d7d2f639f0ad575f92fbee04fa5ad38a
Jason Alfred <jasonalfred75@gmail.com>
2020-10-20 20:43:33 -0500

    added click handlers and linked cloro to index.html for other graphs.

--- static/js/countyMapJason.js
+++ static/js/countyMapJason.js
@@ -1,100 +1,94 @@
-var beeData;
+function cloroplethMap() {
 
-function getBeeData() {
-  let dict = d3.json("/data_api").then(function (data) {
-    beeData = data;
-    console.log(beeData);
+// Need to check if map is already initialized since we are
+// not reloading pages.
+  var container = L.DomUtil.get('map');
+      if(container != null){
+        container._leaflet_id = null;
+      }
 
-  })
-}
+  // Creating map object
+  let mapYear = 2012
+  let myMap = L.map("map", {
+    center: [39.8283, -98.5795],
+    zoom: 4.5
+  });
 
-getBeeData();
-
-
-
-// Creating map object
-
-let mapYear = 2012
-let myMap = L.map("map", {
-  center: [39.8283, -98.5795],
-  zoom: 4.5
-});
-
-// Adding tile layer
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/light-v10",
-  accessToken: API_KEY
-}).addTo(myMap);
-
-// Load in geojson data 
-// Three years available and make 3 calls for layers 2002, 2007, 2012
-let geoData = `/data_CountyData${mapYear}`;
-
-d3.json(geoData).then(function (countyData) {
-  console.log(countyData);
-  L.geoJson(countyData).addTo(myMap);
-  let geojson = L.choropleth(countyData, {
-
-    // Define what  property in the features to use
-    valueProperty: "Density",
-
-    // Set color scale
-    scale: ["#9E1A1A", "#22BC22"],
-
-    // Number of breaks in step range
-    steps: 20,
-
-    // q for quartile, e for equidistant, k for k-means
-    mode: "q",
-    style: {
-      // Border color
-      color: "#fff",
-      weight: 1,
-      fillOpacity: 0.8
-    },
-
-    // Binding a pop-up to each layer
-    onEachFeature: function (feature, layer) {
-      layer.bindPopup("<h2>" + feature.properties.AreaName + "</h2><hr>County Land Area (sq. mi): " +
-        feature.properties.LandArea + "<br>Colonies in County: " + feature.properties.Value +
-        "<br>Colony Density (colonies per sq. mi): " + feature.properties.Density);
-      layer.on('mouseover', function () { layer.openPopup(); });
-      layer.on('mouseout', function () { layer.closePopup(); });
-    }
+  // Adding tile layer
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/light-v10",
+    accessToken: API_KEY
   }).addTo(myMap);
 
+  // Load in geojson data 
+  // Three years available and make 3 calls for layers 2002, 2007, 2012
+  let geoData = `/data_CountyData${mapYear}`;
 
-  // Set up the legend
-  let legend = L.control({ position: "bottomright" });
+  d3.json(geoData).then(function (countyData) {
+    L.geoJson(countyData).addTo(myMap);
+    let geojson = L.choropleth(countyData, {
 
-  legend.onAdd = function () {
-    let div = L.DomUtil.create("div", "info legend");
-    let limits = geojson.options.limits;
-    let colors = geojson.options.colors;
-    let labels = [];
+      // Define what  property in the features to use
+      valueProperty: "Density",
 
-    // Add min & max
-    let legendInfo = `<h1>${mapYear} Bee Colony Density</h1><h4>Colonies per sq. mile</h4>` +
-      "<div class=\"labels\">" +
-      "<div class=\"min\">" + limits[0] + "</div>" +
-      "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-      "</div>";
+      // Set color scale
+      scale: ["#9E1A1A", "#22BC22"],
 
-    div.innerHTML = legendInfo;
+      // Number of breaks in step range
+      steps: 20,
 
-    limits.forEach(function (limit, index) {
-      labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-    });
+      // q for quartile, e for equidistant, k for k-means
+      mode: "q",
+      style: {
+        // Border color
+        color: "#fff",
+        weight: 1,
+        fillOpacity: 0.8
+      },
 
-    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-    return div;
-  };
+      // Binding a pop-up to each layer
+      onEachFeature: function (feature, layer) {
+        layer.bindPopup("<h2>" + feature.properties.AreaName + "</h2><hr>County Land Area (sq. mi): " +
+          feature.properties.LandArea + "<br>Colonies in County: " + feature.properties.Value +
+          "<br>Colony Density (colonies per sq. mi): " + feature.properties.Density);
+        layer.on('mouseover', function () { layer.openPopup(); });
+        layer.on('mouseout', function () { layer.closePopup(); });
+      }
+    }).addTo(myMap);
 
-  // Adding legend to the map
-  legend.addTo(myMap);
 
-});
+    // Set up the legend
+    let legend = L.control({ position: "bottomright" });
+
+    legend.onAdd = function () {
+      let div = L.DomUtil.create("div", "info legend");
+      let limits = geojson.options.limits;
+      let colors = geojson.options.colors;
+      let labels = [];
+
+      // Add min & max
+      let legendInfo = `<h1>${mapYear} Bee Colony Density</h1><h4>Colonies per sq. mile</h4>` +
+        "<div class=\"labels\">" +
+        "<div class=\"min\">" + limits[0] + "</div>" +
+        "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+        "</div>";
+
+      div.innerHTML = legendInfo;
+
+      limits.forEach(function (limit, index) {
+        labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+      });
+
+      div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+      return div;
+    };
+
+    // Adding legend to the map
+    legend.addTo(myMap);
+
+  });
+}
--- static/js/nav.js
+++ static/js/nav.js
@@ -1,7 +1,7 @@
 var beeData;
 
 function getBeeData() {
-    let dict = d3.json("/data_api").then(function(data) {
+    let dict = d3.json("/data_api").then(function (data) {
         beeData = data;
         console.log(beeData);
 
@@ -9,3 +9,36 @@ function getBeeData() {
 }
 
 getBeeData();
+
+function otherMaps() {
+    d3.select("#map").html("<h1>You can put some maps and graphs here!  :)</h1>")
+}
+
+function getData() {
+    d3.select("#map").html("Maybe put the Bee data in here or add more links to call each table?")
+}
+
+d3.select("#graph1")
+    .on("click", function () {
+        // Get current event info
+        otherMaps()
+    });
+
+d3.select("#graph2")
+    .on("click", function () {
+        // Get current event info
+        otherMaps()
+    });
+
+
+d3.select("#graph3")
+    .on("click", function () {
+        // Get current event info
+        cloroplethMap()
+    });
+
+d3.select("#datalink")
+    .on("click", function () {
+        // Get current event info
+        getData()
+    });
